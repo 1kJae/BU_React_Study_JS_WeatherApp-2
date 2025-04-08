bd6ba54234c31171f61cea670a7cc164711a1510
1kJae <wnstjq62@naver.com>
2025-04-08 15:06:56 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { WeatherBox } from './component/WeatherBox.js';
+import { WeatherButton } from './component/WeatherButton';
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
+  const cities = ['paris', 'new york', 'tokyo', 'seoul'];
+  
+  const getCurrentLocation = () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  }
+
+  const getWeatherByCurrentLocation = async(lat, lon) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d714a94abc375fdc877a122ff4402bab&units=metric`
+    setLoading(true);
+    let response = await fetch(url)
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if(city==""){
+      getCurrentLocation();
+    } else {
+      getWeatherByCity();
+    }
+  },[city])
+
+  const getWeatherByCity = async() => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d714a94abc375fdc877a122ff4402bab&units=metric`;
+    setLoading(true);
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <div className='container'>
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+      ) : (
+      <div className='container'>
+        <WeatherBox weather={weather}/>
+        <WeatherButton cities={cities} setCity={setCity}/>
+      </div>
+      )}
     </div>
   );
 }
-
 export default App;
